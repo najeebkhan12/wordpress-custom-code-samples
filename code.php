80f1a66e2cf570ea8f104db34a55c37096ff0541
Najeeb Khan <khannajeeb362@gmail.com>
2024-12-01 22:16:01 +0500

    Woocommerce dynamic pricing

--- code.php
+++ code.php
@@ -56,3 +56,22 @@ function map_wp_role_to_discourse_group($user) {
 }
 
 //Discourse integration End
+
+
+//Woocommerce dynamic pricing
+//This function replaces existing price with Final price custom price
+add_action( 'woocommerce_before_calculate_totals', 'change_product_price_dynamically' );
+
+function change_product_price_dynamically( $cart ) {
+    if ( is_admin() || ! did_action( 'woocommerce_before_calculate_totals' ) ) {
+        return;
+    }
+
+    // Loop through cart items
+    foreach ( $cart->get_cart() as $cart_item ) {
+        $booking_id = $cart_item['hp_booking'];
+        $new_price = hivepress()->cache->get_post_cache( $booking_id, 'price', 'models/booking' ); // Set your custom price
+        $cart_item['data']->set_price( $new_price );
+    }
+}
+//Woocommerce dynamic pricing end
